f726955480c66b92823d94c2aec928b9d1b5d360
Fedor Smirnov <fedorsmirnov89@gmail.com>
2023-09-19 07:47:05 +0200

    Finished chapter 7

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,35 +1,18 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-use sqlx::PgPool;
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+use sqlx::{PgPool, Postgres, Transaction};
 use tracing::error;
 use uuid::Uuid;
 
-use crate::domain::{
-    new_subscriber::NewSubscriber, subscriber_email::SubscriberEmail,
-    subscriber_name::SubscriberName,
+use crate::{
+    domain::{
+        new_subscriber::NewSubscriber, subscriber_email::SubscriberEmail,
+        subscriber_name::SubscriberName,
+    },
+    email_client::EmailClient,
+    startup::ApplicationBaseUrl,
 };
-
-#[tracing::instrument(
-    name = "Adding a new subscriber",
-    skip(form, pool),  // tells tracing to skip these arguments (default is to attach all function arguments to the context of the span)
-    fields(
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    )
-)]
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let new_subscriber = match form.0.try_into() {
-        Ok(s) => s,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    match insert_subscriber(&pool, &new_subscriber).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            error!("failed to executed query: {e:?}");
-            HttpResponse::InternalServerError().finish()
-        }
-    }
-}
 
 impl TryFrom<FormData> for NewSubscriber {
     type Error = String;
@@ -41,35 +24,145 @@ impl TryFrom<FormData> for NewSubscriber {
     }
 }
 
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
 #[tracing::instrument(
-    name = "saving new subscriber details in the database",
-    skip(new_subscriber, pool)
+    name = "Adding a new subscriber",
+    skip(form, pool, email_client, base_url),  // tells tracing to skip these arguments (default is to attach all function arguments to the context of the span)
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
 )]
-async fn insert_subscriber(
-    pool: &PgPool,
-    new_subscriber: &NewSubscriber,
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+    base_url: web::Data<ApplicationBaseUrl>,
+) -> HttpResponse {
+    let new_subscriber = match form.0.try_into() {
+        Ok(s) => s,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    let mut transaction = match pool.begin().await {
+        Ok(transaction) => transaction,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    let subscriber_id = match insert_subscriber(&mut transaction, &new_subscriber).await {
+        Ok(id) => id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    let subscription_token = generate_subscription_token();
+
+    if store_token(&mut transaction, subscriber_id, &subscription_token)
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+
+    if transaction.commit().await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+
+    if send_confirmation_email(
+        &email_client,
+        new_subscriber,
+        &base_url.0,
+        &subscription_token,
+    )
+    .await
+    .is_err()
+    {
+        HttpResponse::InternalServerError().finish()
+    } else {
+        HttpResponse::Ok().finish()
+    }
+}
+
+#[tracing::instrument(
+    name = "Store subscription token in the database",
+    skip(subscription_token, transaction)
+)]
+async fn store_token(
+    transaction: &mut Transaction<'_, Postgres>,
+    subscriber_id: Uuid,
+    subscription_token: &str,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        r#"
-            INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-            VALUES ($1, $2, $3, $4, 'confirmed')
-        "#,
-        Uuid::new_v4(),
-        new_subscriber.email.as_ref(),
-        new_subscriber.name.as_ref(),
-        Utc::now()
+        r#"INSERT INTO subscription_tokens (subscription_token, subscriber_id)
+        VALUES ($1, $2)"#,
+        subscription_token,
+        subscriber_id
     )
-    .execute(pool)
+    .execute(transaction)
     .await
     .map_err(|e| {
-        error!("failed to execute query {e:?}");
+        tracing::error!("Failed to execute query: {e:?}");
         e
     })?;
     Ok(())
 }
 
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    email: String,
-    name: String,
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber, base_url)
+)]
+async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+    base_url: &str,
+    subscription_token: &str,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link =
+        format!("{base_url}/subscriptions/confirm?subscription_token={subscription_token}");
+    let plain_body = &format!("Welcome. Have a link: <a href=\"{confirmation_link}\">here</a>");
+    let html_body = &format!("Welcome. Have a link: {confirmation_link}");
+
+    email_client
+        .send_email(new_subscriber.email, "Welcome", plain_body, html_body)
+        .await
+}
+
+fn generate_subscription_token() -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat_with(|| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(25)
+        .collect()
+}
+
+#[tracing::instrument(
+    name = "saving new subscriber details in the database",
+    skip(new_subscriber, transaction)
+)]
+async fn insert_subscriber(
+    transaction: &mut Transaction<'_, Postgres>,
+    new_subscriber: &NewSubscriber,
+) -> Result<Uuid, sqlx::Error> {
+    let subscriber_id = Uuid::new_v4();
+    sqlx::query!(
+        r#"
+            INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+            VALUES ($1, $2, $3, $4, 'pending_confirmation')
+        "#,
+        subscriber_id,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
+        Utc::now()
+    )
+    .execute(transaction)
+    .await
+    .map_err(|e| {
+        error!("failed to execute query {e:?}");
+        e
+    })?;
+    Ok(subscriber_id)
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,10 @@
 use std::{net::TcpListener, time::Duration};
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{
+    dev::Server,
+    web::{self, Data},
+    App, HttpServer,
+};
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use tracing::info;
 use tracing_actix_web::TracingLogger;
@@ -8,7 +12,9 @@ use tracing_actix_web::TracingLogger;
 use crate::{
     configuration::{DataBaseSettings, Settings},
     email_client::EmailClient,
-    routes::{health_check::health_check, subscriptions::subscribe},
+    routes::{
+        health_check::health_check, subscriptions::subscribe, subscriptions_confirm::confirm,
+    },
 };
 
 pub struct Application {
@@ -43,7 +49,12 @@ impl Application {
             port = configuration.application.port
         );
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection, email_client)?;
+        let server = run(
+            listener,
+            connection,
+            email_client,
+            configuration.application.base_url,
+        )?;
         let application = Self { server, port };
         Ok(application)
     }
@@ -63,20 +74,27 @@ pub async fn get_connection_pool(configuration: &DataBaseSettings) -> PgPool {
         .connect_lazy_with(configuration.with_db())
 }
 
+// We need this one to provide the url as app context
+pub struct ApplicationBaseUrl(pub String);
+
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
+    base_url: String,
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
+    let base_url = Data::new(ApplicationBaseUrl(base_url));
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
+            .app_data(base_url.clone())
     })
     .listen(listener)?
     .run();
